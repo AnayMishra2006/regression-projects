8239827bfe9d5a47edf09854a5d723e73240e385
AnayMishra2006 <anaymishra20212105@gmail.com>
2025-08-14 11:31:13 +0530

    learning deployment

---
+++ application.py
@@ -0,0 +1,20 @@
+import pickle
+from flask import Flask,request,jsonify,render_template
+import pandas as pd 
+import numpy as np 
+from sklearn.preprocessing import StandardScaler
+
+
+application=Flask(__name__)
+app=application
+
+# import ridge regressor and standard scaler pickle
+ridge_model=pickle.load(open('models/ridge.pkl','rb'))
+standardScaler=pickle.load(open('models/scaler.pkl','rb'))
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+if __name__="__main__"
+    app.run(host="0.0.0.0")
